97298a7a1606fb13a25cc8128ab52a43773d4ad6
raotnameh <raotnameh@gmail.com>
2020-05-24 14:06:30 +0530

    -

--- LSTM_CRF_NER/embeddings/get_fasttext_embeddings.sh
+++
@@ -1,4 +0,0 @@
-wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.zip
-#wget https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki-news-300d-1M-subword.vec.zip
-unzip wiki.en.zip
-rm wiki.en.zip
